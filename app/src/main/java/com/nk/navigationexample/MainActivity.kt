ee7b543cfdca5f19cdd3773fba0d874dad0af4f2
n-k-d <n.k.d.assist@gmail.com>
2021-04-03 16:22:37 +0530

    adding toolbar

--- app/src/main/java/com/nk/navigationexample/MainActivity.kt
+++ app/src/main/java/com/nk/navigationexample/MainActivity.kt
@@ -2,10 +2,35 @@ package com.nk.navigationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolBar))
+
+        val navController = findNavController(R.id.fragment)
+        val config = AppBarConfiguration(navController.graph)
+
+        findViewById<Toolbar>(R.id.toolBar).setupWithNavController(navController, config)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragment)
+
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
